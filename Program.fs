4ffd05ff55977dbfc6ace829c11198f63c97dcd6
Mark McFadden <m2web@yahoo.com>
2023-06-25 20:35:24 -0400

    feat: :fire: Initial commit

---
+++ Program.fs
@@ -0,0 +1,13 @@
+﻿//write a function that takes a string and returns a string
+let reverseString (s: string) =
+    new string(Array.rev (s.ToCharArray()))
+
+[<EntryPoint>]
+let main argv =
+    //let str = "Mark McFadden!!"
+    System.Console.WriteLine("Enter a string: ")
+    let str = System.Console.ReadLine()
+
+    //instead of reversing the string, display it as is
+    printfn "You entered: %s" str
+    0 //return an integer exit code
